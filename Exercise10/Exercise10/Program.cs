7aa981cd9418fafbe32c00d5cdd1c5ed9cfaf5c9
TheHiddenOne <145466606+TheHiddenOne-Mges@users.noreply.github.com>
2024-03-20 08:19:15 -0400

    (Step 1 Complete)
    Created Basic Menu
    
    Time is limited and their is no getting it back so spend it wisely.

---
+++ Exercise10/Exercise10/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //Manages Wether the menu should be active or not
+            int menuChoice = 0;
+
+            while (menuChoice != 4)
+            {
+                // The Menu
+                Console.WriteLine("*****************");
+                Console.WriteLine("Menu :");
+                Console.WriteLine("1- Start New Save");
+                Console.WriteLine("2- Load Old Save");
+                Console.WriteLine("3- Options");
+                Console.WriteLine("4- Quit");
+                Console.WriteLine("*****************");
+                //Where the user input modifies the value of menuChoice.
+                menuChoice = int.Parse(Console.ReadLine());
+                // Prevents the allowcation of an invalid paramater
+                if (menuChoice <= 0 || menuChoice >= 5)
+                {
+                    Console.WriteLine("Please Insert a Valid Option");
+                    menuChoice = int.Parse(Console.ReadLine());
+                }
+                //Actual results for the menu
+                else if (menuChoice == 4)
+                {
+                    Console.WriteLine("Closing Program...");
+                }
+
+                else if (menuChoice == 3)
+                {
+                    Console.WriteLine("Error: 404 Options Page Not Found");
+                }
+                else if (menuChoice == 2)
+                {
+                    Console.WriteLine("Loading Preveious Game Save... What Save Data You Haven't Player This Game.");
+                }
+                else if (menuChoice == 1) 
+                {
+                    Console.WriteLine("Loading Game... What Game?");
+                }
+
+            }
+
+
+
+
+        }
+}   }
